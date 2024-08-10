3d73975ff8ad1ca49fc322bbfb62d6ad35a48bad
ExamKim <23633041.thi@student.iuh.edu.vn>
2024-08-10 20:02:01 +0700

    Message

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Thi Chiu!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
